6aac4c9c8f2169096079b138f6a8d4ef1f36a3a3
Nate Levesque <nate@thenaterhood.com>
2025-06-15 18:39:13 -0400

    add test for cname injection

--- server/dns_server_test.go
+++ server/dns_server_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/thenaterhood/spuddns/metrics"
 	"github.com/thenaterhood/spuddns/models"
 	"github.com/thenaterhood/spuddns/resolver"
+	"github.com/thenaterhood/spuddns/system"
 )
 
 func getAppState(cache cache.Cache) *app.AppState {
@@ -33,7 +34,7 @@ func getAppState(cache cache.Cache) *app.AppState {
 	}
 }
 
-func runDnsServerTest(t *testing.T, appCfg app.AppConfig, appState app.AppState, clientId *string) *dns.Msg {
+func runDnsServerTest(t *testing.T, appCfg app.AppConfig, appState app.AppState, clientId *string, qName string) *dns.Msg {
 	server := NewDnsServer(appCfg, appState)
 
 	waitLock := sync.Mutex{}
@@ -48,7 +49,7 @@ func runDnsServerTest(t *testing.T, appCfg app.AppConfig, appState app.AppState,
 	waitLock.Lock()
 
 	q, err := models.NewDnsQueryFromQuestions(
-		[]dns.Question{{Name: "example.com.", Qtype: dns.TypeA}},
+		[]dns.Question{{Name: qName, Qtype: dns.TypeA}},
 	)
 	if err != nil {
 		t.Fatalf("invalid dns question: %v", err)
@@ -63,7 +64,7 @@ func runDnsServerTest(t *testing.T, appCfg app.AppConfig, appState app.AppState,
 	addr := server.standard_dns_server.PacketConn.LocalAddr().String() // Get address via the PacketConn that gets set.
 	r, _, err := c.Exchange(m, addr)
 	if err != nil {
-		t.Fatal("failed to exchange example.com", err)
+		t.Fatal("failed to exchange", "name", qName, "err", err)
 	}
 
 	return r
@@ -75,10 +76,53 @@ func TestServerResolvesBasicQuery(t *testing.T) {
 	appCfg.BindAddress = ""
 	appCfg.DnsServerPort = 0
 
-	r := runDnsServerTest(t, appCfg, *getAppState(&cache.DummyCache{}), nil)
+	r := runDnsServerTest(t, appCfg, *getAppState(&cache.DummyCache{}), nil, "example.com.")
 	a := r.Answer[0].(*dns.A).A
 	if a.String() != "127.0.0.1" {
 		t.Error("unexpected result for example.com", a.String(), "!= 127.0.0.1")
+	}
+}
+
+func TestServerResolvesWithCNAMEIfNotFullyQualified(t *testing.T) {
+
+	appCfg := app.GetDefaultConfig()
+	appCfg.BindAddress = ""
+	appCfg.DnsServerPort = 0
+
+	appCfg.ResolvConf = &system.ResolvConf{
+		Search: []string{"com"},
+	}
+
+	r := runDnsServerTest(t, appCfg, *getAppState(&cache.DummyCache{}), nil, "example.")
+
+	foundA := false
+	foundCname := false
+
+	for _, rr := range r.Answer {
+		answer, err := models.NewDnsAnswerFromRR(rr)
+		if err != nil {
+			t.Fatalf("unexpected error getting DNS answer from RR: %v", err)
+		}
+
+		if answer.Type == dns.TypeA {
+			if answer.Data != "127.0.0.1" {
+				t.Error("unexpected A RR for example.com", answer.Data, "!= 127.0.0.1")
+			}
+			foundA = true
+		} else if answer.Type == dns.TypeCNAME {
+			if answer.Data != "example.com." {
+				t.Error("unexpected CNAME RR for example.", answer.Data, "!= example.com.")
+			}
+			foundCname = true
+		}
+	}
+
+	if !foundCname {
+		t.Error("CNAME was not added")
+	}
+
+	if !foundA {
+		t.Error("A record was missing")
 	}
 }
 
@@ -88,7 +132,7 @@ func TestServerResolvesEdnsQueryAuthNotRequired(t *testing.T) {
 	appCfg.BindAddress = ""
 	appCfg.DnsServerPort = 0
 
-	r := runDnsServerTest(t, appCfg, *getAppState(&cache.DummyCache{}), nil)
+	r := runDnsServerTest(t, appCfg, *getAppState(&cache.DummyCache{}), nil, "example.com.")
 
 	a := r.Answer[0].(*dns.A).A
 	if a.String() != "127.0.0.1" {
@@ -107,7 +151,7 @@ func TestServerResolvesEdnsQueryBadAuth(t *testing.T) {
 	}
 
 	cpeId := "test"
-	r := runDnsServerTest(t, appCfg, *getAppState(&cache.DummyCache{}), &cpeId)
+	r := runDnsServerTest(t, appCfg, *getAppState(&cache.DummyCache{}), &cpeId, "example.com.")
 
 	if r.Rcode != dns.RcodeRefused {
 		t.Errorf("expected opcode REFUSED (%d), got %d", dns.RcodeRefused, r.Rcode)
@@ -128,7 +172,7 @@ func TestServerResolvesEdnsQueryGoodAuth(t *testing.T) {
 	}
 
 	cpeId := "test"
-	r := runDnsServerTest(t, appCfg, *getAppState(&cache.DummyCache{}), &cpeId)
+	r := runDnsServerTest(t, appCfg, *getAppState(&cache.DummyCache{}), &cpeId, "example.com.")
 
 	a := r.Answer[0].(*dns.A).A
 	if a.String() != "127.0.0.1" {
@@ -146,7 +190,7 @@ func TestServerRejectsNonEdnsQueryWhenAuthRequired(t *testing.T) {
 		"example": {},
 	}
 
-	r := runDnsServerTest(t, appCfg, *getAppState(&cache.DummyCache{}), nil)
+	r := runDnsServerTest(t, appCfg, *getAppState(&cache.DummyCache{}), nil, "example.com.")
 
 	if r.Rcode != dns.RcodeRefused {
 		t.Errorf("expected opcode REFUSED (%d), got %d", dns.RcodeRefused, r.Rcode)
